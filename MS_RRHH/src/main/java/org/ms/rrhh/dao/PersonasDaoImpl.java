ead7946d516149dd6adf6d4d1cb2afdad5a7374e
eliu diaz <eliudiaz@gmail.com>
2016-10-25 06:20:49 -0600

    configurations fixed

--- MS_RRHH/src/main/java/org/ms/rrhh/dao/PersonasDaoImpl.java
+++ MS_RRHH/src/main/java/org/ms/rrhh/dao/PersonasDaoImpl.java
@@ -5,10 +5,45 @@
  */
 package org.ms.rrhh.dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.ms.rrhh.domain.model.Persona;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author edcracken
  */
-public class PersonasDaoImpl {
-    
+@Repository
+public class PersonasDaoImpl implements PersonasDao {
+
+    protected EntityManager entityManager;
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    @Override
+    public List<Persona> getAll() throws DataAccessException {
+        Query query = getEntityManager().createQuery("select c from Car c");
+        List<Persona> resultList = query.getResultList();
+        return resultList;
+    }
+
+    @Transactional
+    @Override
+    public Persona getOne(Long carId) throws DataAccessException {
+        return getEntityManager().find(Persona.class, carId);
+    }
+
 }
