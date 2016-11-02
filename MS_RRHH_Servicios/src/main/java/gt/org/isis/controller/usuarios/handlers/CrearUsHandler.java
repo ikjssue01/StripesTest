a35b43f4553da614f6b7ecfc31e14e9ec401d1a7
edcracken <eliudiaz@gmail.com>
2016-11-02 00:36:46 -0600

    rol validation

--- MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/usuarios/handlers/CrearUsHandler.java
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/usuarios/handlers/CrearUsHandler.java
@@ -6,9 +6,10 @@
 package gt.org.isis.controller.usuarios.handlers;
 
 import gt.org.isis.api.AbstractRequestHandler;
+import static gt.org.isis.api.ValidationsHelper.isNull;
+import gt.org.isis.api.misc.exceptions.ExceptionsManager;
 import gt.org.isis.controller.dto.UsuarioDto;
 import gt.org.isis.converters.UsuarioDtoConverter;
-import gt.org.isis.model.Persona;
 import gt.org.isis.model.Role;
 import gt.org.isis.model.Usuario;
 import gt.org.isis.model.utils.EntitiesHelper;
@@ -35,19 +36,27 @@ public class CrearUsHandler extends AbstractRequestHandler<UsuarioDto, UsuarioDt
 
     @Override
     public UsuarioDto execute(final UsuarioDto request) {
-//        Persona persona = personas.findOne(request.getCui());
+
         Role role = roles.findOne(request.getRoleId());
+        if (isNull(role)) {
+            throw ExceptionsManager.newValidationException("invalid_role",
+                    new String[]{"role_id,Role es invalido o no existe"});
+        }
         UsuarioDtoConverter bc;
         final Usuario r = (bc = new UsuarioDtoConverter()).toEntity(request);
         r.setClave(new String(DigestUtils.md5Digest(request.getClave().getBytes())));
-//        r.setFkPersona(persona);
+
         r.setFkRole(role);
         r.setId(request.getUsuario());
         EntitiesHelper.setDateCreateRef(r);
         r.setCreadoPor("test");
+
         UsuarioDto us = bc.toDTO(usuarios.save(r));
         us.setClave(null);
         us.setConfirmacionClave(null);
+
+        //        Persona persona = personas.findOne(request.getCui());
+//        r.setFkPersona(persona);
         return us;
     }
 
