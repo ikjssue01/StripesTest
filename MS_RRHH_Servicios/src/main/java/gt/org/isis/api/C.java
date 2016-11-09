0bcca79daaebf48b2af710622258b32ff4c1a34c
edcracken <eliudiaz@gmail.com>
2016-11-09 00:21:00 -0600

    catalogs constant definition

--- MS_RRHH_Servicios/src/main/java/gt/org/isis/api/C.java
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/api/C.java
@@ -12,5 +12,15 @@ package gt.org.isis.api;
 public class C {
 
     public static final String EMPTY = "";
-    public static final String CAT_TIPO_MUNICIPIOS = "MUNICIPIOS";
+    public static final String CAT_NACIONALIDAD = "NACIONALIDAD";
+    public static final String CAT_IDIOMA = "IDIOMA";
+    public static final String CAT_TIPO_MUNICIPIOS = "MUNICIPIO";
+    public static final String CAT_TIPO_DEPTOS = "DEPARTAMENTOS";
+    public static final String CAT_TIPO_PAISES = "PAIS";
+    public static final String CAT_PUESTO_FUNCIONAL = "PUESTO_FUNCIONAL";
+    public static final String CAT_PUESTO_RENGLON = "RENGLONES";
+    public static final String CAT_PUESTO_NOMINAL = "PUESTO_NOMINAL";
+    public static final String CAT_UN_DISTRITO = "DISTRITO";
+    public static final String CAT_UN_LUGAR_ESPEC = "LUGAR_ESPECIFICO";
+    public static final String CAT_UN_COMUNIDAD = "COMUNIDAD";
 }
---
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/dto/GetPersonaDto.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.org.isis.controller.dto;
+
+/**
+ *
+ * @author eliud
+ */
+public class GetPersonaDto extends PersonaDto {
+
+    private Integer fkPaisNac;
+    private String fkPaisNacNombre;
+    private Integer fkDepartamentoNac;
+    private String fkDepartamentoNacNombre;
+    private Integer fkPaisVecindad;
+    private String fkPaisVecindadNombre;
+    private Integer fkDepartamentoVecindad;
+    private String fkDepartamentoVecindadNombre;
+    private Integer fkPaisCedula;
+    private String fkPaisCedulaNombre;
+    private Integer fkDepartamentoCedula;
+    private String fkDepartamentoCedulaNombre;
+
+    public Integer getFkPaisNac() {
+        return fkPaisNac;
+    }
+
+    public void setFkPaisNac(Integer fkPaisNac) {
+        this.fkPaisNac = fkPaisNac;
+    }
+
+    public String getFkPaisNacNombre() {
+        return fkPaisNacNombre;
+    }
+
+    public void setFkPaisNacNombre(String fkPaisNacNombre) {
+        this.fkPaisNacNombre = fkPaisNacNombre;
+    }
+
+    public Integer getFkDepartamentoNac() {
+        return fkDepartamentoNac;
+    }
+
+    public void setFkDepartamentoNac(Integer fkDepartamentoNac) {
+        this.fkDepartamentoNac = fkDepartamentoNac;
+    }
+
+    public String getFkDepartamentoNacNombre() {
+        return fkDepartamentoNacNombre;
+    }
+
+    public void setFkDepartamentoNacNombre(String fkDepartamentoNacNombre) {
+        this.fkDepartamentoNacNombre = fkDepartamentoNacNombre;
+    }
+
+    public Integer getFkPaisVecindad() {
+        return fkPaisVecindad;
+    }
+
+    public void setFkPaisVecindad(Integer fkPaisVecindad) {
+        this.fkPaisVecindad = fkPaisVecindad;
+    }
+
+    public String getFkPaisVecindadNombre() {
+        return fkPaisVecindadNombre;
+    }
+
+    public void setFkPaisVecindadNombre(String fkPaisVecindadNombre) {
+        this.fkPaisVecindadNombre = fkPaisVecindadNombre;
+    }
+
+    public Integer getFkDepartamentoVecindad() {
+        return fkDepartamentoVecindad;
+    }
+
+    public void setFkDepartamentoVecindad(Integer fkDepartamentoVecindad) {
+        this.fkDepartamentoVecindad = fkDepartamentoVecindad;
+    }
+
+    public String getFkDepartamentoVecindadNombre() {
+        return fkDepartamentoVecindadNombre;
+    }
+
+    public void setFkDepartamentoVecindadNombre(String fkDepartamentoVecindadNombre) {
+        this.fkDepartamentoVecindadNombre = fkDepartamentoVecindadNombre;
+    }
+
+    public Integer getFkPaisCedula() {
+        return fkPaisCedula;
+    }
+
+    public void setFkPaisCedula(Integer fkPaisCedula) {
+        this.fkPaisCedula = fkPaisCedula;
+    }
+
+    public String getFkPaisCedulaNombre() {
+        return fkPaisCedulaNombre;
+    }
+
+    public void setFkPaisCedulaNombre(String fkPaisCedulaNombre) {
+        this.fkPaisCedulaNombre = fkPaisCedulaNombre;
+    }
+
+    public Integer getFkDepartamentoCedula() {
+        return fkDepartamentoCedula;
+    }
+
+    public void setFkDepartamentoCedula(Integer fkDepartamentoCedula) {
+        this.fkDepartamentoCedula = fkDepartamentoCedula;
+    }
+
+    public String getFkDepartamentoCedulaNombre() {
+        return fkDepartamentoCedulaNombre;
+    }
+
+    public void setFkDepartamentoCedulaNombre(String fkDepartamentoCedulaNombre) {
+        this.fkDepartamentoCedulaNombre = fkDepartamentoCedulaNombre;
+    }
+
+}
