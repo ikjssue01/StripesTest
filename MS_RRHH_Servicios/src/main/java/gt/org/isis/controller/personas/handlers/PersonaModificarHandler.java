bd909410310b738c07c7d323534e266ea5e309ed
edcracken <eliudiaz@gmail.com>
2016-11-09 00:07:22 -0600

    update fixed

--- MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/PersonaModificarHandler.java
+++ MS_RRHH_Servicios/src/main/java/gt/org/isis/controller/personas/handlers/PersonaModificarHandler.java
@@ -5,10 +5,13 @@
  */
 package gt.org.isis.controller.personas.handlers;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import gt.org.isis.api.AbstractValidationsRequestHandler;
 import gt.org.isis.controller.dto.EstudioSaludDto;
 import gt.org.isis.controller.dto.IdiomaDto;
 import gt.org.isis.controller.dto.PersonaDto;
+import gt.org.isis.controller.dto.RegistroLaboralPuestoDto;
 import gt.org.isis.controller.dto.ReqModPersonaDto;
 import gt.org.isis.converters.DpiDtoConverter;
 import gt.org.isis.converters.EstudiosSaludConverter;
@@ -18,12 +21,14 @@ import gt.org.isis.converters.PersonaDtoConverter;
 import gt.org.isis.converters.PersonaHistorialConverter;
 import gt.org.isis.converters.RegistroAcademicoConverter;
 import gt.org.isis.converters.RegistroLaboralConverter;
+import gt.org.isis.converters.RegistroLaboralPuestosConverter;
 import gt.org.isis.model.Dpi;
 import gt.org.isis.model.EstudioSalud;
 import gt.org.isis.model.HistoricoPersona;
 import gt.org.isis.model.Idioma;
 import gt.org.isis.model.LugarResidencia;
 import gt.org.isis.model.Persona;
+import gt.org.isis.model.Puesto;
 import gt.org.isis.model.RegistroAcademico;
 import gt.org.isis.model.RegistroLaboral;
 import gt.org.isis.model.enums.EstadoVariable;
@@ -36,6 +41,7 @@ import gt.org.isis.repository.LugarResidenciaRepository;
 import gt.org.isis.repository.PersonasRepository;
 import gt.org.isis.repository.RegistroAcademicoRepository;
 import gt.org.isis.repository.RegistroLaboralRepository;
+import java.util.ArrayList;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -77,6 +83,7 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
         actualizaDpi(p, r);
         actualizaLugarResidencia(p, r);
         BeanUtils.copyProperties(r, p);
+        p.setEdad(EntitiesHelper.getAge(r.getFechaNacimiento()));
         repo.save(p);
         return true;
     }
@@ -96,6 +103,7 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
         idiomasRepo.deleteInBatch(p.getIdiomaCollection());
         for (IdiomaDto t : r.getIdiomas()) {
             Idioma i = new IdiomaDtoConverter().toEntity(t);
+            i.setFkPersona(p);
             EntitiesHelper.setDateCreateRef(i);
             idiomasRepo.save(i);
         }
@@ -105,6 +113,7 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
         HistoricoPersona hp = new PersonaHistorialConverter()
                 .toEntity(new PersonaDtoConverter().toDTO(p));
         hp.setFkPersona(p);
+        hp.setEdad(p.getEdad());
         EntitiesHelper.setDateCreateRef(hp);
         hp.setCreadoPor("admin");
         historicoRepo.save(hp);
@@ -123,13 +132,27 @@ public class PersonaModificarHandler extends AbstractValidationsRequestHandler<R
 
     private void actualizaRegistroLaboral(Persona p, PersonaDto r) {
         rLabRepository.archivarRegitro(p.getCui());
-        RegistroLaboral ra = new RegistroLaboralConverter()
+        final RegistroLaboral rl = new RegistroLaboralConverter()
                 .toEntity(r.getRegistroLaboral());
-        ra.setEstado(EstadoVariable.ACTUAL);
-        ra.setFkPersona(p);
-        ra.setCreadoPor(p.getCreadoPor());
-        EntitiesHelper.setDateCreateRef(ra);
-        rLabRepository.save(ra);
+        rl.setEstado(EstadoVariable.ACTUAL);
+        rl.setFkPersona(p);
+        rl.setCreadoPor(p.getCreadoPor());
+        EntitiesHelper.setDateCreateRef(rl);
+
+        rl.setPuestoCollection(new ArrayList<Puesto>(Collections2
+                .transform(r.getRegistroLaboral().getPuestos(),
+                        new Function<RegistroLaboralPuestoDto, Puesto>() {
+                    @Override
+                    public Puesto apply(RegistroLaboralPuestoDto f) {
+                        Puesto ps = new RegistroLaboralPuestosConverter().toEntity(f);
+                        ps.setFkRegistroLaboral(rl);
+                        EntitiesHelper.setDateCreateRef(ps);
+                        ps.setCreadoPor("admin");
+                        return ps;
+                    }
+                })));
+
+        rLabRepository.save(rl);
     }
 
     private void actualizaDpi(Persona p, PersonaDto r) {
